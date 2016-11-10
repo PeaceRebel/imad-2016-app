b3b3e8bd4f80bf8d9944d744ce4493ca51c68d96
Bipin <PeaceRebel@users.noreply.github.com>
2016-11-10 17:51:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,5 @@
 console.log('Loaded!');
 
-var button = document.getElementById('counter');
-
-button.onclick = function (){
-  //create request object
-  var request = new XMLHttpRequest();
-  
-  //capture response and store
-  request.onreadystatechange = function (){
-    if(request.readyState == XMLHttpRequest.DONE){
-        if(request.status == 200){
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-    }  
-  };
-  //make request
-  request.open('GET', 'http://peacerebel.imad.hasura-app.io/counter', true);
-  request.send(null);
-};
-
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     //create request object
