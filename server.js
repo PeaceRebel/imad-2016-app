cf0bc4b6e7d2ddd2de3d42f181fd50aa46cbdc7b
Bipin <PeaceRebel@users.noreply.github.com>
2016-11-10 18:21:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.post('/create-user', function (req, res) {
    var password = req.body.password;
    var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password, salt);
-   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, res) {
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
@@ -94,13 +94,31 @@ app.post('/create-user', function (req, res) {
 });
 
 
-var counter = 0;
-
 app.post('/login', function(req, res){
    var username = req.body.username;
    var password = req.body.password;
    
-   pool.query('SELECT * FROM "user" WHERE username=$1',[username], funstion(err,res))
+   pool.query('SELECT * FROM "user" WHERE username=$1',[username], function(err,res){
+      if (err){
+          res.status(500).send(err.toString());
+      }
+      else {
+          if (result.rows.length === 0){
+              res.status(403).send('username/password is invalid!');
+          }
+          else{
+              var dbString = result.rows[0].password;
+              var salt= dbString.split('$')[2];
+              var hashedPassword = hash(password, salt);
+              if (hashedPassword == dbString){
+                  res.send('Credentials accepted!');
+              }
+              else{
+                  res.status(403).send('username/password is invalid!');
+              }
+          }
+      }
+   });
 });
 
 var names = [];
