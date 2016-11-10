f8339bd7209687b6e76a9d708609ea12b1a7d343
Bipin <PeaceRebel@users.noreply.github.com>
2016-11-10 17:04:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ app.get('/hash/:input', function (req, res){
     res.send(hashedString);
 });
 
-app.get('/createuser', function (req, res) {
+app.get('/create-user', function (req, res) {
    // username, password
    // {"username": "tanmai", "password": "password"}
    // JSON
