f6204307d7c7d2e306771b122e3814aedeb1070e
Bipin <PeaceRebel@users.noreply.github.com>
2016-11-10 21:23:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var pool = new Pool(config);
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({ extended: false }));
 
 function createTemplate (data){
     var title = data.title;
